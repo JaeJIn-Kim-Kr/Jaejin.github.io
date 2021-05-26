d4287868044be1fd5ecf4636b23be81ea5c439a2
JJKim <rlawowls1012@gmail.com>
2021-05-26 23:14:44 +0900

    migrate

---
+++ database/migrations/2021_05_26_140304_create_tasks_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->bigIncrements('num');
+            $table->string('title');
+            $table->string('content');
+            $table->string('user_id');
+            $table->string('waste_Chk');
+            $table->string('progress_Check');
+
+            $table->foreign('user_id')->reference('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+}
