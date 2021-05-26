ac7598018d323e2bcd3352a5ade6c101fc7c309d
JJKim <rlawowls1012@gmail.com>
2021-05-26 22:55:06 +0900

    Committte

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -94,11 +94,6 @@
                     <a href="https://github.com/laravel/laravel">GitHub</a>
                 </div>
             </div>
-            <div>
-                @foreach($val as $vals)
-                    {{$vals}}
-                @endforeach
-            </div>
         </div>
         
     </body>
