5aa1dc4bc6c29a5a768504308cc01bdd98491da3
InfoKimjj <kjj1012@inforang.com>
2021-05-27 13:00:10 +0900

    git pull Test

--- routes/web.php
+++ routes/web.php
@@ -18,14 +18,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
-Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/todoList', function(){
+    return view('/todoList');
+});
