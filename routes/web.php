e04afe4d4cb3f0ae5a50af1a1d94e8f7d987a1ac
InfoKimjj <kjj1012@inforang.com>
2021-06-01 08:56:42 +0900

    Web.php File comments

--- routes/web.php
+++ routes/web.php
@@ -20,13 +20,13 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 
 Route::middleware('auth')->group(function(){
-    Route::get('/todoList', 'TaskController@index');
-    Route::get('/create', 'TaskController@create_View');
-    Route::get('/tasks/view/{num}', 'TaskController@View');
-    Route::post('/create_todoList', 'TaskController@todoList');
-    Route::delete('/delete/{num}', 'TaskController@delete');
-    Route::get('/edit/{num}', 'TaskController@edit');
-    Route::put('/update/{num}', 'TaskController@update');
+    Route::get('/todoList',         'TaskController@index');        // Todo List Main Page
+    Route::get('/create',           'TaskController@create_View');  // Todo List Create Page
+    Route::get('/tasks/view/{num}', 'TaskController@View');         // Todo List Detail Page
+    Route::post('/create_todoList', 'TaskController@todoList');     // Todo List Create
+    Route::delete('/delete/{num}',  'TaskController@delete');       // Todo List Delete
+    Route::get('/edit/{num}',       'TaskController@edit');         // Todo List Edit Page
+    Route::put('/update/{num}',     'TaskController@update');       // Todo List Update
 });
 Auth::routes();
 
