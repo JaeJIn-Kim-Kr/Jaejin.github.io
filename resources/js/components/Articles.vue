cf42869f90114697ba8f23f81edd5ea4c793f526
InfoKimjj <kjj1012@inforang.com>
2021-06-14 13:01:14 +0900

    Study Vue..

--- resources/js/components/Articles.vue
+++ resources/js/components/Articles.vue
@@ -1,5 +1,8 @@
 <template>
     <div>
         <h2>Articles</h2>
+        <p>
+            TodoList 에서 container 클래스를 가진 div 자식 articles 가 template가 될것이다. 해당 vue 문서에 있는것이 articles 에 표시된다.
+        </p>
     </div>
 </template>
